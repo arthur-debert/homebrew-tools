1bc7c0d428a33ff5d60011a388260a25c3039e2b
shipit release <shipit-release@users.noreply.github.com>
2026-08-15 08:17:26 +0000

    dodot 5.5.1

--- Formula/dodot.rb
+++ Formula/dodot.rb
@@ -1,22 +1,22 @@
 class Dodot < Formula
   desc "A dotfiles manager that uses symlinks for live editing"
   homepage "https://github.com/arthur-debert/dodot"
-  version "5.5.0"
+  version "5.5.1"
   license "MIT"
 
   on_macos do
-    url "https://github.com/arthur-debert/dodot/releases/download/v5.5.0/dodot-aarch64-apple-darwin.tar.gz"
-    sha256 "916a33611d106c04f292eba7b080ffc6b646eba5c2e48cda61c71a6080b98424"
+    url "https://github.com/arthur-debert/dodot/releases/download/v5.5.1/dodot-aarch64-apple-darwin.tar.gz"
+    sha256 "6ef1f18002ad1e3639424ff89ab68f44f4ddf22f6ea37220bf6a1e786a311aae"
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/arthur-debert/dodot/releases/download/v5.5.0/dodot-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "cdd0e998795c27d794f1c16c832e0239c273e0e34b598d511763923eea3eae50"
+      url "https://github.com/arthur-debert/dodot/releases/download/v5.5.1/dodot-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "7a8dd5f9643ff7ec31d4376be66183b6f29d636c7dca8c25816d0f062abc17a8"
     end
     on_intel do
-      url "https://github.com/arthur-debert/dodot/releases/download/v5.5.0/dodot-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "9f52c1a362e5130b7c1412dad2704a5df39c4bcb64e10e010c9c9602ca6193e2"
+      url "https://github.com/arthur-debert/dodot/releases/download/v5.5.1/dodot-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "71286ab49860d75c71a418d697f267e7c6316b3993166e3842bf38a16dd368a3"
     end
   end
 
